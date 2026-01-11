866b7dc6e9c8b0419d40e7365ba800b3bfe88d20
hanshul22 <hanshulkumawat22@gmail.com>
2026-01-11 19:28:29 +0530

    folder structure and setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AppRoutes from '@/routes/AppRoutes.jsx';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <AppRoutes />
+        {/* <h1 className="text-3xl font-bold underline">Hello, world!</h1> */}
+    </div>
+  );
+};
+
+export default App;
